de26c5be9ef745b29d3d708603bb0f8e216c8af6
Roberto <robertomedeirosgarcia@gmail.com>
2017-11-19 23:38:57 -0200

    Layout

--- src/pages/AppMain.vue
+++ src/pages/AppMain.vue
@@ -1,29 +1,32 @@
 <template>
   <v-content class="grey lighten-4">
-    <v-toolbar color="light-blue darken-4" dark>
-      <v-toolbar-title>Vue Status</v-toolbar-title>
+    <div class="scroll" :class="fixed" v-scroll="onScroll">
+    <v-toolbar dark class="toolbar">
+      <v-toolbar-title>
+        Vue Status
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>search</v-icon>
       </v-btn>
       <v-menu v-model="menu">
-          <v-btn icon slot="activator" dark>
-            <v-icon>more_vert</v-icon>
-          </v-btn>
-          <v-list>
-            <v-list-tile @click='logOut'>
-              <v-list-tile-action>
-                <v-btn icon ripple>
-                  <v-icon color="grey lighten-1">exit_to_app</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-              <v-list-tile-title>Sair</v-list-tile-title>
-            </v-list-tile>
-          </v-list>
+        <v-btn icon slot="activator" dark>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile @click='logOut'>
+            <v-list-tile-action>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">exit_to_app</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+            <v-list-tile-title>Sair</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
       </v-menu>
     </v-toolbar>
-    <v-tabs fixed centered class="app-tabs">
-    <v-tabs-bar color="teal accent-3" :class="tabsbarclass">
+    <v-tabs fixed centered>
+    <v-tabs-bar color="teal accent-3" class="tabs" :class="tabsbarclass">
       <v-tabs-slider color="green"></v-tabs-slider>
       <v-tabs-item :to="{name:'AppMainPosts'}" router>
         Posts
@@ -31,11 +34,13 @@
       <v-tabs-item :to="{name:'AppMainNews'}" router>
         News
       </v-tabs-item>
-    </v-tabs-bar>
-      <v-container v-scroll="onScroll" :class="containerclass">
+    </v-tabs-bar>  
+      </v-tabs>
+    </div>    
+       <v-container class="container">
         <router-view />
       </v-container>
-    </v-tabs>
+    
     </v-content>
 
 </template>
@@ -46,19 +51,16 @@ export default {
     return {
       menu: false,
       text: 'asdfa dsfasdfads fa',
-      tabsbarclass: '',
-      containerclass: ''
+      fixed: ''
     }
   },
   methods: {
     onScroll (e) {
       const offsetTop = window.pageYOffset || document.documentElement.scrollTop
-      if (offsetTop > 56) {
-        this.tabsbarclass = 'tabsfixed'
-        this.containerclass = 'containerfixed'
+      if (offsetTop > 60) {
+        this.fixed = 'fixed'
       } else {
-        this.tabsbarclass = ''
-        this.containerclass = ''
+        this.fixed = ''
       }
     },
     logOut () {
@@ -70,14 +72,23 @@ export default {
 }
 </script>
 
-<style lang="stylus">
-.tabsfixed
-  position fixed
+<style lang="stylus" scoped>
+.toolbar
+  height 100px
+  background-color #1c3c59 !important
+.scroll
   top 0
   left 0
+  right 0
+  position fixed
+  transition: transform .4s
+  transform: translateZ(0)
   z-index 5
-.containerfixed
+.fixed
+  transform: translate3d(0,-100px,0)
+
+.container
   .layout
-    padding-top 50px
+    padding-top 150px
 </style>
 
